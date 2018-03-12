6faf9e0c8cd2b7b7e314dafaebeab69112da51b4
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-03-12 14:22:56 +0000

    # SSKTS-868 ### Added - ユーザー本人確認後のトリガーを追加。

--- openPecorinoAccount/dst/index.js
+++ openPecorinoAccount/dst/index.js
@@ -12,11 +12,11 @@ process.env.PATH = `${process.env.PATH}: ${process.env.LAMBDA_TASK_ROOT}`;
 const pecorino = require("@motionpicture/pecorino-domain");
 const mongooseConnectionOptions_1 = require("./mongooseConnectionOptions");
 /**
- * サインアップ前のトリガー
+ * ユーザー本人確認後のトリガー
  * eventのサンプル
  * {
   "version": 1,
-  "triggerSource": "PreSignUp_SignUp",
+  "triggerSource": "PostConfirmation_ConfirmSignUp",
   "region": "<region>",
   "userPoolId": "<userPoolId>",
   "userName": "<userName>",
@@ -26,46 +26,33 @@ const mongooseConnectionOptions_1 = require("./mongooseConnectionOptions");
       ...
   },
   "request": {
-      "userAttributes": {
+      "userAttributes" : {
           "email": "<email>",
           "phone_number": "<phone_number>",
           ...
-       },
-      "validationData": {
-          "k1": "v1",
-          "k2": "v2",
-          ...
-       }
+      }
   },
-  "response": {
-      "autoConfirmUser": false
-  }
+  "response": {}
 }
  */
 exports.handler = (event, context) => __awaiter(this, void 0, void 0, function* () {
     // This Lambda function returns a flag to indicate if a user should be auto-confirmed.
     // Perform any necessary validations.
     // Pecorino口座作成
-    const INITIAL_BALANCE = (process.env.INITIAL_BALANCE !== undefined) ? parseInt(process.env.INITIAL_BALANCE, 10) : 0;
-    yield pecorino.mongoose.connect(process.env.MONGOLAB_URI, mongooseConnectionOptions_1.default);
-    const accountRepo = new pecorino.repository.Account(pecorino.mongoose.connection);
-    yield pecorino.service.account.open({
-        id: `${event.userName}`,
-        name: `${event.request.userAttributes.family_name} ${event.request.userAttributes.given_name}`,
-        initialBalance: INITIAL_BALANCE
-    })({ account: accountRepo });
-    // Impose a condition that the minimum length of the username of 5 is imposed on all user pools.
-    // if (event.userName.length < 5) {
-    //     var error = new Error('failed!');
-    //     context.done(error, event);
-    // }
-    // Access your resource which contains the list of emails of users who were invited to sign up
-    // Compare the list of email IDs from the request to the approved list
-    // if (event.userPoolId === "yourSpecialUserPool") {
-    //     if (event.request.userAttributes.email in listOfEmailsInvited) {
-    //         event.response.autoConfirmUser = true;
-    //     }
-    // }
-    // Return result to Cognito
-    context.done(null, event);
+    try {
+        const INITIAL_BALANCE = (process.env.INITIAL_BALANCE !== undefined) ? parseInt(process.env.INITIAL_BALANCE, 10) : 0;
+        yield pecorino.mongoose.connect(process.env.MONGOLAB_URI, mongooseConnectionOptions_1.default);
+        const accountRepo = new pecorino.repository.Account(pecorino.mongoose.connection);
+        yield pecorino.service.account.openIfNotExists({
+            id: `sskts-${event.userName}`,
+            name: `${event.request.userAttributes.family_name} ${event.request.userAttributes.given_name}`,
+            initialBalance: INITIAL_BALANCE
+        })({ account: accountRepo });
+        yield pecorino.mongoose.disconnect();
+        // Return result to Cognito
+        context.done(null, event);
+    }
+    catch (error) {
+        context.done(error, event);
+    }
 });
---
+++ openPecorinoAccount/src/index.ts
@@ -0,0 +1,54 @@
+process.env.PATH = `${process.env.PATH}: ${process.env.LAMBDA_TASK_ROOT}`;
+
+import * as pecorino from '@motionpicture/pecorino-domain';
+
+import mongooseConnectionOptions from './mongooseConnectionOptions';
+
+/**
+ * ユーザー本人確認後のトリガー
+ * eventのサンプル
+ * {
+  "version": 1,
+  "triggerSource": "PostConfirmation_ConfirmSignUp",
+  "region": "<region>",
+  "userPoolId": "<userPoolId>",
+  "userName": "<userName>",
+  "callerContext": {
+      "awsSdk": "<calling aws sdk with version>",
+      "clientId": "<apps client id>",
+      ...
+  },
+  "request": {
+      "userAttributes" : {
+          "email": "<email>",
+          "phone_number": "<phone_number>",
+          ...
+      }
+  },
+  "response": {}
+}
+ */
+export const handler = async (event: any, context: any) => {
+    // This Lambda function returns a flag to indicate if a user should be auto-confirmed.
+
+    // Perform any necessary validations.
+
+    // Pecorino口座作成
+    try {
+        const INITIAL_BALANCE = (process.env.INITIAL_BALANCE !== undefined) ? parseInt(process.env.INITIAL_BALANCE, 10) : 0;
+        await pecorino.mongoose.connect(<string>process.env.MONGOLAB_URI, mongooseConnectionOptions)
+        const accountRepo = new pecorino.repository.Account(pecorino.mongoose.connection);
+        await pecorino.service.account.openIfNotExists({
+            id: `sskts-${event.userName}`,
+            name: `${event.request.userAttributes.family_name} ${event.request.userAttributes.given_name}`,
+            initialBalance: INITIAL_BALANCE
+        })({ account: accountRepo });
+
+        await pecorino.mongoose.disconnect();
+
+        // Return result to Cognito
+        context.done(null, event);
+    } catch (error) {
+        context.done(error, event);
+    }
+};
